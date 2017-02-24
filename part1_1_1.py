d7efd802bd62ba9459669efaad03f60ca263569f
Braden Watling <braden.watling@mail.utoronto.ca>
2017-02-24 02:22:56 -0500

    Finished 1.1.1

--- part1_1_1.py
+++ part1_1_1.py
@@ -3,14 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def cross_entropy(M, w, b, l, x, y):
-    loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(tf.reduce_sum(tf.reduce_sum(w * x, 1), 1) + b, tf.transpose(y)), 1) / M
+    loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(tf.expand_dims(tf.reduce_sum(tf.reduce_sum(x * w, -1), -1), -1) + b, y), 0) / M
     weight_decay = l / 2 * tf.reduce_sum(tf.reduce_sum(tf.square(w), 0), 0)
 
     return loss + weight_decay
 
 def accuracy(M, w, b, x, y):
     # Predict 1 if value is > 0.5, 0 otherwise
-    prediction = tf.to_float(tf.greater(tf.reduce_sum(tf.reduce_sum(w * x, 1), 1) + b, 0.5))
+    prediction = tf.to_float(tf.greater(tf.reduce_sum(tf.reduce_sum(x * w, 1), 1) + b, 0.5))
 
     # 1 - abs(prediction - y)
     # Correct prediction => 1, incorrect prediction => 0
@@ -35,8 +35,6 @@ if __name__ == '__main__':
         validData, validTarget = Data[3500:3600], Target[3500:3600]
         testData, testTarget = Data[3600:], Target[3600:]
 
-        print(trainTarget.shape)
-
         dataInput = tf.placeholder(tf.float32)
         targetInput = tf.placeholder(tf.float32)
 
@@ -44,7 +42,7 @@ if __name__ == '__main__':
         M = trainData.shape[0]
         N = trainData.shape[1]
 
-        w = tf.Variable(tf.zeros([1, N]), dtype=tf.float32)
+        w = tf.Variable(tf.zeros([N, N]), dtype=tf.float32)
         b = tf.Variable(0, dtype=tf.float32)
         learning_rate = 0.01
         weight_decay = 0.01
@@ -63,8 +61,8 @@ if __name__ == '__main__':
         trainingAccuracies = []
         testingLosses = []
         testingAccuracies = []
-        for j in range(10):
-            for i in range(0, trainData.shape[0], batch_size):
+        for j in range(100):
+            for i in range(0, M, batch_size):
                 # Perform the update
                 _, err, accuracy = sess.run([optimizer, error, acc], feed_dict={
                             dataInput: trainData[i:i+batch_size-1],
@@ -78,21 +76,18 @@ if __name__ == '__main__':
                          })
                 testingLosses.append(err)
                 testingAccuracies.append(accuracy)
-        print(w.eval(sess))
-        print(b.eval(sess))
+        #print(w.eval(sess))
+        #print(b.eval(sess))
         plt.figure(1)
         plt.plot(trainingLosses)
         plt.title('SGD Logistic Regression Training Loss vs. Number of Updates')
-        plt.show()
         plt.figure(2)
         plt.plot(testingLosses)
         plt.title('SGD Logistic Regression Testing Loss vs. Number of Updates')
-        plt.show()
         plt.figure(3)
         plt.plot(trainingAccuracies)
         plt.title('SGD Logistic Regression Training Classification Accuracy vs. Number of Updates')
-        plt.show()
         plt.figure(4)
-        plt.plot(trainingLosses)
+        plt.plot(testingAccuracies)
         plt.title('SGD Logistic Regression Testing Classification Accuracy vs. Number of Updates')
         plt.show()
